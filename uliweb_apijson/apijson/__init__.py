f6133f8a97e9158877aaf7057e99b363a208979b
Chunlin Zhang <zhangchunlin@gmail.com>
2019-05-09 13:42:01 +0800

    add functions.get_apijson_table

--- uliweb_apijson/apijson/__init__.py
+++ uliweb_apijson/apijson/__init__.py
@@ -21,3 +21,29 @@ def get_apijson_tables(role="UNKNOWN"):
                     editable = role in roles
             c["editable"] = editable
     return apijson_tables
+
+def get_apijson_table(role="UNKNOWN",name=None):
+    from uliweb import settings
+
+    if not name:
+        return {}
+    s = settings.APIJSON_TABLES
+    if s:
+        apijson_tables = dict(s.iteritems())
+    else:
+        return {}
+    
+    c = apijson_tables.get(name)
+    if not c:
+        return {}
+    editable = c.get("editable",False)
+    _model_name = c.get("@model_name") or n
+    if editable=="auto":
+        editable = False
+        POST = settings.APIJSON_MODELS.get(_model_name,{}).get("POST")
+        if POST:
+            roles = POST["roles"]
+            if roles:
+                editable = role in roles
+        c["editable"] = editable
+    return c
